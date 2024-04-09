0506b32cb1043189008234265179bfd983464f88
Hoo93 <sksk8922@gmail.com>
2024-04-10 00:22:02 +0900

    add: 시간,요일 생성 로직 추가

--- server/src/attendances/attendances.module.ts
+++ server/src/attendances/attendances.module.ts
@@ -4,9 +4,10 @@ import { AttendancesController } from './attendances.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendance } from './entities/attendance.entity';
 import { UserAttendance } from './entities/user-attendance.entity';
+import { AttendanceDay } from './entities/attendance-day.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance, UserAttendance])],
+  imports: [TypeOrmModule.forFeature([Attendance, UserAttendance, AttendanceDay])],
   controllers: [AttendancesController],
   providers: [AttendancesService],
 })
