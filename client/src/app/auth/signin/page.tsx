9334fa427ddf64d370e06ba5414a5ed0c992a724
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-04-23 23:35:20 +0900

    Fix: 로그인 페이지 수정

--- client/src/app/auth/signin/page.tsx
+++ client/src/app/auth/signin/page.tsx
@@ -78,7 +78,7 @@ const Index = () => {
         <ContainerST>
             <StyledBoxST>
                 <LoginTypographyST>로그인</LoginTypographyST>
-                <Box
+                {/* <Box
                     component="form"
                     display={'flex'}
                     flexDirection={'column'}
@@ -99,10 +99,10 @@ const Index = () => {
                         onChange={(e) => onChange('password', e.target.value)}
                         inputProps={TextFiledInputProps}
                     />
-                </Box>
+                </Box> */}
             </StyledBoxST>
             <BoxSTLoginCommon gap={'38px'}>
-                <BoxSTLoginCommon gap={'4px'}>
+                {/* <BoxSTLoginCommon gap={'4px'}>
                     <BoxSTLoginMaintain>
                         <StyledCheckbox
                             inputProps={{
@@ -144,15 +144,16 @@ const Index = () => {
                             </StyledLinkTypography>
                         </Box>
                     </BoxSTLoginCommon>
-                </BoxSTLoginCommon>
-                <Box display={'flex'} justifyContent={'space-between'}>
-                    <StyledKakaoLoginButton
-                        onClick={() => {
-                            alert('준비중인 기능입니다.');
-                        }}
-                    >
-                        카카오 로그인
-                    </StyledKakaoLoginButton>
+                </BoxSTLoginCommon> */}
+                <StyledKakaoLoginButton
+                    onClick={() => {
+                        alert('준비중인 기능입니다.');
+                    }}
+                >
+                    카카오 로그인
+                </StyledKakaoLoginButton>
+                {/* <Box display={'flex'} justifyContent={'space-between'}>
+                 
                     <StyledNaverLoginButton
                         onClick={() => {
                             alert('준비중인 기능입니다.');
@@ -160,7 +161,7 @@ const Index = () => {
                     >
                         네이버 로그인
                     </StyledNaverLoginButton>
-                </Box>
+                </Box> */}
             </BoxSTLoginCommon>
 
             <Image
@@ -199,7 +200,7 @@ const ContainerST = styled(Container)(() => {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100%',
-        gap: '20px',
+        gap: '32px',
     };
 });
 
@@ -288,8 +289,8 @@ const BoxSTLoginMaintain = styled(Box)(() => {
 // 카카오 및 네이버 로그인 버튼 스타일
 const StyledLoginButton = styled(Box)(() => {
     return {
-        width: '152px',
-        height: '40px',
+        width: '313px',
+        height: '39px',
         borderRadius: '20px',
         fontSize: '14px',
         lineHeight: '19.07px',
