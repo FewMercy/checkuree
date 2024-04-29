013aa0a1a703359330984cde867066c0d5c3a09e
Hoo93 <sksk8922@gmail.com>
2024-04-29 23:01:18 +0900

    delete: 백엔드 빌드파일 삭제

--- server/dist/common/decorator/current-ip.decorator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const CurrentIp: (...dataOrPipes: unknown[]) => ParameterDecorator;
--- server/dist/file-manager/image-processor.service.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="multer" />
-/// <reference types="node" />
-export declare class ImageProcessorService {
-    constructor();
-    resize(file: Express.Multer.File, maxLength: number): Promise<Buffer>;
-}
