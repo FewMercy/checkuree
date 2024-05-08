3bb028b9e7262fd5307283710ae2177621b98b2c
Hoo93 <sksk8922@gmail.com>
2024-05-08 10:21:02 +0900

    fix: 배열 속성 검증안되는 오류 수정

--- server/src/schedules/dto/create-schedule.dto.ts
+++ server/src/schedules/dto/create-schedule.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { Schedule } from '../entities/schedule.entity';
 import { SingleSchedule } from '../const/single-schedule.class';
+import { Type } from 'class-transformer';
+import { SingleRecord } from '../../records/const/singleRecord.class';
 
 export class CreateScheduleDto {
   @IsString()
@@ -23,6 +25,8 @@ export class CreateScheduleDto {
   attendeeId: string;
 
   @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SingleSchedule)
   @ApiProperty({ description: '출석 요일과 시간', type: Array.of(SingleSchedule) })
   singleSchedules: SingleSchedule[];
 
