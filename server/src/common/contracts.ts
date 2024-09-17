3ebfcfe6a3f6b1e8bfdea4321dd5c3c72dd94bcc
Hoo93 <sksk8922@gmail.com>
2024-09-17 23:31:47 +0900

    edit: 불필요 타입 삭제

--- server/src/common/contracts.ts
+++ server/src/common/contracts.ts
@@ -1,12 +1,4 @@
 /**
- * 출석 체크를 위한 YYMMDD 형식의 날짜
- * @deprecated
- *
- * @example '240909' // 2024년 9월 9일
- */
-export type YyMmDd = string;
-
-/**
  * 출석 체크를 위한 YYYY-MM-DD 형식의 날짜
  *
  * @example '2024-09-09' // 2024년 9월 9일
