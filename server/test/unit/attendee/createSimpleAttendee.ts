68b7ed2d904a2b9a43988c86b8b19d83cb7ef8f8
Hoo93 <sksk8922@gmail.com>
2024-09-19 23:49:36 +0900

    edit: 매개변수 타입 명시, 주석 추가

--- server/test/unit/attendee/createSimpleAttendee.ts
+++ server/test/unit/attendee/createSimpleAttendee.ts
@@ -1,9 +1,15 @@
 import { Attendee } from '../../../src/attendees/entities/attendee.entity';
+import { Gender } from '../../../src/attendees/const/gender.enum';
 
-export function createSimpleAttendee(name, attendanceId, createId) {
+/**
+ * name, attendanceId, createId 를 입력 받아 간단한 attendee 생성
+ * gender = MALE 로 통일
+ */
+export function createSimpleAttendee(name: string, attendanceId: string, createId: string) {
   const attendee = new Attendee();
   attendee.name = name;
   attendee.attendanceId = attendanceId;
   attendee.createId = createId;
+  attendee.gender = Gender.MALE;
   return attendee;
 }
