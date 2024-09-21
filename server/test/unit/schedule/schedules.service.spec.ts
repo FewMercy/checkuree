cc1c887ff4967b81fb082bb15303b15df9bbd105
Sang Hoo Park <sksk8922@gmail.com>
2024-09-22 01:42:56 +0900

    fix: 깨진 테스트코드 수정

--- server/test/unit/schedule/schedules.service.spec.ts
+++ server/test/unit/schedule/schedules.service.spec.ts
@@ -6,7 +6,6 @@ import { Attendee } from '../../../src/attendees/entities/attendee.entity';
 import { User } from '../../../src/users/entities/user.entity';
 import { Attendance } from '../../../src/attendances/entities/attendance.entity';
 import { Schedule } from '../../../src/schedules/entities/schedule.entity';
-import { AttendanceType } from '../../../src/attendances/const/attendance-type.enum';
 import { DayType } from '../../../src/schedules/const/day-type.enum';
 import { CreateScheduleDto } from '../../../src/schedules/dto/create-schedule.dto';
 import { BadRequestException } from '@nestjs/common';
@@ -16,6 +15,8 @@ import { Record } from '../../../src/records/entities/record.entity';
 import { AttendanceStatus } from '../../../src/records/const/record-type.enum';
 import { createAttendee } from '../attendee/createAttendee';
 import { SingleSchedule } from '../../../src/schedules/const/single-schedule.class';
+import { ScheduleFilterDto } from '../../../src/schedules/dto/schedule-filter.dto';
+import { Gender } from '../../../src/attendees/const/gender.enum';
 
 describe('SchedulesService', () => {
   let module: TestingModule;
@@ -88,9 +89,11 @@ describe('SchedulesService', () => {
 
       // Then
       sut.map((schedule) => {
-        expect(schedule.attendeeId).toBe('Attendee Id 1');
-        expect(schedule.day).toBe('MONDAY');
-        expect(schedule.time).toBe('1000');
+        expect(schedule).toMatchObject({
+          attendeeId: 'Attendee Id 1',
+          day: 'MON',
+          time: '1000',
+        });
       });
     });
 
@@ -117,8 +120,8 @@ describe('SchedulesService', () => {
 
       // Then
       expect(sut).toHaveLength(2);
-      expect(sut.some((schedule) => schedule.day === 'TUESDAY' && schedule.time === '1800')).toBeTruthy();
-      expect(sut.some((schedule) => schedule.day === 'WEDNESDAY' && schedule.time === '1900')).toBeTruthy();
+      expect(sut.some((schedule) => schedule.day === DayType.TUESDAY && schedule.time === '1800')).toBeTruthy();
+      expect(sut.some((schedule) => schedule.day === DayType.WEDNESDAY && schedule.time === '1900')).toBeTruthy();
     });
 
     it('입력한 시간이 0000~2400 범위에 있지 않으면 오류를 일으킨다.', async () => {
@@ -193,12 +196,11 @@ describe('SchedulesService', () => {
 
       const schedule_1 = createSchedule('Attendee Id 1', DayType.MONDAY, '1230');
       const schedule_2 = createSchedule('Attendee Id 2', DayType.TUESDAY, '1330');
-
       const schedule_3 = createSchedule('Attendee Id 3', DayType.WEDNESDAY, '1430');
       await scheduleRepository.insert([schedule_1, schedule_2, schedule_3]);
 
       // When
-      const sut = await service.findAllByAttendanceId(targetAttendanceId);
+      const sut = await service.findAllByAttendanceId(targetAttendanceId, ScheduleFilterDto.of());
 
       // Then
       expect(sut.count).toBe(2);
@@ -208,7 +210,56 @@ describe('SchedulesService', () => {
     });
   });
 
-  describe('findTodayScheduleByAttendanceId', () => {
+  describe('findScheduleByAttendanceIdAndDate', () => {
+    it('스케쥴의 time 으로 grouped 된 schedule 과 record 를 리턴한다.', async () => {
+      // Given
+      const targetAttendanceId = 'testAttendanceId';
+
+      const attendee_1 = new Attendee();
+      attendee_1.id = 'Attendee Id 1';
+      attendee_1.attendanceId = targetAttendanceId;
+
+      const attendee_2 = new Attendee();
+      attendee_2.id = 'Attendee Id 2';
+      attendee_2.attendanceId = 'notTestAttendanceId';
+
+      const attendee_3 = new Attendee();
+      attendee_3.id = 'Attendee Id 3';
+      attendee_3.attendanceId = targetAttendanceId;
+
+      const schedule_1 = createSchedule('Attendee Id 1', DayType.MONDAY, '1230');
+      const schedule_2 = createSchedule('Attendee Id 2', DayType.TUESDAY, '1330');
+      const schedule_3 = createSchedule('Attendee Id 3', DayType.WEDNESDAY, '1430');
+      await scheduleRepository.insert([schedule_1, schedule_2, schedule_3]);
+
+      const record_1 = new Record();
+      record_1.attendeeId = attendee_1.id;
+      record_1.status = AttendanceStatus.PRESENT;
+      record_1.date = '2024-02-05';
+      record_1.time = '1230';
+      record_1.day = DayType.MONDAY;
+      record_1.createId = 'user id 1';
+
+      const record_2 = new Record();
+      record_2.attendeeId = attendee_1.id;
+      record_2.status = AttendanceStatus.PRESENT;
+      record_2.date = '2024-02-06';
+      record_2.time = '1330';
+      record_2.day = DayType.TUESDAY;
+      record_2.createId = 'user id 1';
+
+      await recordRepository.insert([record_1, record_2]);
+
+      // When
+      // 2024-02-05는 월요일
+      const sut = await service.findScheduleByAttendanceIdAndDate(targetAttendanceId, '2024-02-05', ScheduleFilterDto.of());
+
+      // Then
+      expect(sut.count).toBe(1);
+      // 2024-02-05는 월요일 : 1230 schedule 만 포함되어야 함
+      expect(Object.keys(sut.items[0])).toMatchObject(['1230']);
+    });
+
     it('출석부에 스케쥴 중 조회날짜의 요일과의 스케줄과 조회 날짜의 record를 리턴한다.', async () => {
       // Given
       const targetAttendanceId = 'testAttendanceId';
@@ -234,6 +285,7 @@ describe('SchedulesService', () => {
       record_1.attendeeId = attendee_1.id;
       record_1.status = AttendanceStatus.PRESENT;
       record_1.date = '2024-02-05';
+      record_1.time = '1230';
       record_1.day = DayType.MONDAY;
       record_1.createId = 'user id 1';
 
@@ -241,17 +293,19 @@ describe('SchedulesService', () => {
       record_2.attendeeId = attendee_1.id;
       record_2.status = AttendanceStatus.PRESENT;
       record_2.date = '2024-02-06';
+      record_2.time = '1230';
       record_2.day = DayType.TUESDAY;
       record_2.createId = 'user id 1';
 
       await recordRepository.insert([record_1, record_2]);
 
       // When
-      const sut = await service.findTodayScheduleByAttendanceId(targetAttendanceId, new Date('2024-02-05'));
+      // 2024-02-05는 월요일
+      const sut = await service.findScheduleByAttendanceIdAndDate(targetAttendanceId, '2024-02-05', ScheduleFilterDto.of());
 
       // Then
       expect(sut.count).toBe(1);
-      sut.items.map((schedule) => {
+      sut.items[0]['1230'].map((schedule) => {
         expect(schedule.attendee.attendanceId).toBe(targetAttendanceId);
         expect(schedule.day).toBe(DayType.MONDAY);
         expect(schedule?.attendee.records).toBeDefined();
@@ -265,19 +319,19 @@ describe('SchedulesService', () => {
 
       await attendeeRepository.query('DELETE FROM attendee;');
 
-      const attendee_1 = createAttendee('name_1', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_1 = createAttendee('name_1', targetAttendanceId, 'description', 'user id 1');
       attendee_1.id = 'Attendee Id 1';
 
-      const attendee_2 = createAttendee('name_2', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_2 = createAttendee('name_2', targetAttendanceId, 'description', 'user id 1');
       attendee_2.id = 'Attendee Id 2';
 
-      const attendee_3 = createAttendee('name_3', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_3 = createAttendee('name_3', targetAttendanceId, 'description', 'user id 1');
       attendee_3.id = 'Attendee Id 3';
 
-      const attendee_4 = createAttendee('name_4', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_4 = createAttendee('name_4', targetAttendanceId, 'description', 'user id 1');
       attendee_4.id = 'Attendee Id 4';
 
-      const attendee_5 = createAttendee('name_5', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_5 = createAttendee('name_5', targetAttendanceId, 'description', 'user id 1');
       attendee_5.id = 'Attendee Id 5';
 
       await attendeeRepository.save([attendee_1, attendee_2, attendee_3, attendee_4, attendee_5]);
@@ -290,7 +344,7 @@ describe('SchedulesService', () => {
       await scheduleRepository.insert([schedule_1, schedule_2, schedule_3, schedule_4, schedule_5]);
 
       // When
-      const sut = await service.findTodayScheduleByAttendanceId(targetAttendanceId, new Date('2024-02-05'));
+      const sut = await service.findScheduleByAttendanceIdAndDate(targetAttendanceId, '2024-02-05', ScheduleFilterDto.of());
 
       // Then
       expect(sut.count).toBe(5);
@@ -308,19 +362,19 @@ describe('SchedulesService', () => {
 
       await attendeeRepository.query('DELETE FROM attendee;');
 
-      const attendee_1 = createAttendee('name_1', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_1 = createAttendee('name_1', targetAttendanceId, 'description', 'user id 1');
       attendee_1.id = 'Attendee Id 1';
 
-      const attendee_2 = createAttendee('name_2', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_2 = createAttendee('name_2', targetAttendanceId, 'description', 'user id 1');
       attendee_2.id = 'Attendee Id 2';
 
-      const attendee_3 = createAttendee('name_3', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_3 = createAttendee('name_3', targetAttendanceId, 'description', 'user id 1');
       attendee_3.id = 'Attendee Id 3';
 
-      const attendee_4 = createAttendee('name_4', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_4 = createAttendee('name_4', targetAttendanceId, 'description', 'user id 1');
       attendee_4.id = 'Attendee Id 4';
 
-      const attendee_5 = createAttendee('name_5', targetAttendanceId, 'description', 15, 'user id 1');
+      const attendee_5 = createAttendee('name_5', targetAttendanceId, 'description', 'user id 1');
       attendee_5.id = 'Attendee Id 5';
 
       await attendeeRepository.save([attendee_1, attendee_2, attendee_3, attendee_4, attendee_5]);
@@ -333,61 +387,13 @@ describe('SchedulesService', () => {
       await scheduleRepository.insert([schedule_1, schedule_2, schedule_3, schedule_4, schedule_5]);
 
       // When
-      const sut = await service.findTodayScheduleByAttendanceId(targetAttendanceId, new Date('2024-02-05'));
+      const sut = await service.findScheduleByAttendanceIdAndDate(targetAttendanceId, '2024-02-05', ScheduleFilterDto.of());
 
       // Then
       expect(sut.count).toBe(5);
-      const isOrderedByTimeAscend = sut.items.every((schedule, index, array) => {
+      const isOrderedByTimeAscend = sut.items[0]['2220'].every((schedule, index, array) => {
         if (index === 0) return true;
         return schedule.time >= array[index - 1].time && schedule.attendee.name >= array[index - 1].attendee.name;
-      });
-
-      expect(isOrderedByTimeAscend).toBeTruthy();
-    });
-
-    it('스케쥴을 time,attendee.name 오름차순으로 정렬한다.', async () => {
-      // Given
-      const targetAttendanceId = 'testAttendanceId';
-
-      await attendeeRepository.query('DELETE FROM attendee;');
-
-      const attendee_1 = createAttendee('name_1', targetAttendanceId, 'description', 15, 'user id 1');
-      attendee_1.id = 'Attendee Id 1';
-
-      const attendee_2 = createAttendee('name_2', targetAttendanceId, 'description', 15, 'user id 1');
-      attendee_2.id = 'Attendee Id 2';
-
-      const attendee_3 = createAttendee('name_3', targetAttendanceId, 'description', 15, 'user id 1');
-      attendee_3.id = 'Attendee Id 3';
-
-      const attendee_4 = createAttendee('name_4', targetAttendanceId, 'description', 15, 'user id 1');
-      attendee_4.id = 'Attendee Id 4';
-
-      const attendee_5 = createAttendee('name_5', targetAttendanceId, 'description', 15, 'user id 1');
-      attendee_5.id = 'Attendee Id 5';
-
-      await attendeeRepository.save([attendee_1, attendee_2, attendee_3, attendee_4, attendee_5]);
-
-      const schedule_1 = createSchedule('Attendee Id 1', DayType.MONDAY, '2359');
-      const schedule_2 = createSchedule('Attendee Id 2', DayType.MONDAY, '0000');
-      const schedule_3 = createSchedule('Attendee Id 3', DayType.MONDAY, '1200');
-      const schedule_4 = createSchedule('Attendee Id 4', DayType.MONDAY, '1200');
-      const schedule_5 = createSchedule('Attendee Id 5', DayType.MONDAY, '0900');
-      await scheduleRepository.insert([schedule_1, schedule_2, schedule_3, schedule_4, schedule_5]);
-
-      // When
-      const sut = await service.findTodayScheduleByAttendanceId(targetAttendanceId, new Date('2024-02-05'));
-
-      // Then
-      expect(sut.count).toBe(5);
-      const isOrderedByTimeAscend = sut.items.every((schedule, index, array) => {
-        if (index === 0) return true;
-
-        if (schedule.time > array[index - 1].time) return true;
-        else if (schedule.time === array[index - 1].time) {
-          return schedule.attendee.name >= array[index - 1].attendee.name;
-        }
-        return false;
       });
 
       expect(isOrderedByTimeAscend).toBeTruthy();
@@ -441,7 +447,8 @@ describe('SchedulesService', () => {
     attendance_1.id = 'testAttendanceId';
     attendance_1.title = 'testAttendanceTitle';
     attendance_1.description = 'description';
-    attendance_1.type = AttendanceType.WEEKDAY;
+    attendance_1.availableFrom = '1200';
+    attendance_1.availableTo = '1800';
     attendance_1.createId = 'user id 1';
     attendance_1.createdAt = new Date();
 
@@ -449,7 +456,8 @@ describe('SchedulesService', () => {
     attendance_2.id = 'notTestAttendanceId';
     attendance_2.title = 'testAttendanceTitle2';
     attendance_2.description = 'description';
-    attendance_2.type = AttendanceType.WEEKDAY;
+    attendance_2.availableFrom = '1200';
+    attendance_2.availableTo = '1800';
     attendance_2.createId = 'user id 1';
     attendance_2.createdAt = new Date();
 
@@ -458,25 +466,25 @@ describe('SchedulesService', () => {
     const attendee_1 = new Attendee();
     attendee_1.id = 'Attendee Id 1';
     attendee_1.name = 'Attendee Name 1';
+    attendee_1.gender = Gender.MALE;
     attendee_1.attendanceId = attendance_1.id;
     attendee_1.description = 'Attendee 1 description';
-    attendee_1.age = 10;
     attendee_1.createId = user_1.id;
 
     const attendee_2 = new Attendee();
     attendee_2.id = 'Attendee Id 2';
     attendee_2.name = 'Attendee Name 2';
+    attendee_2.gender = Gender.MALE;
     attendee_2.attendanceId = attendance_2.id;
     attendee_2.description = 'Attendee 2 description';
-    attendee_2.age = 20;
     attendee_2.createId = user_1.id;
 
     const attendee_3 = new Attendee();
     attendee_3.id = 'Attendee Id 3';
     attendee_3.name = 'Attendee Name 2';
+    attendee_3.gender = Gender.MALE;
     attendee_3.attendanceId = attendance_1.id;
     attendee_3.description = 'Attendee 2 description';
-    attendee_3.age = 20;
     attendee_3.createId = user_1.id;
 
     // await attendeeRepository.save
