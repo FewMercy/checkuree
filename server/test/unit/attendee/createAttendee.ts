61bab963c63d55cd341722c1820b4b17ceebc344
Sang Hoo Park <sksk8922@gmail.com>
2024-09-21 23:15:52 +0900

    feat: 기본 gender = MALE 생성하도록 수정

--- server/test/unit/attendee/createAttendee.ts
+++ server/test/unit/attendee/createAttendee.ts
@@ -1,11 +1,15 @@
 import { Attendee } from '../../../src/attendees/entities/attendee.entity';
+import { Gender } from '../../../src/attendees/const/gender.enum';
 
-export function createAttendee(name, attendanceId, description, age, createId) {
+/**
+ * Gender = MALE 인 Attendee 를 생성합니다.
+ */
+export function createAttendee(name: string, attendanceId: string, description: string, age: number, createId: string) {
   const attendee = new Attendee();
   attendee.name = name;
+  attendee.gender = Gender.MALE;
   attendee.attendanceId = attendanceId;
   attendee.description = description;
-  attendee.age = age;
   attendee.createId = createId;
   return attendee;
 }
